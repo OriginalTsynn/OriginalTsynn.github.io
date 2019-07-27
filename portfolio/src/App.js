2975c8ec5275d93955fd8bb70368b23c48580024
Nathan Rotach <sangunetsynn@gmail.com>
2019-07-27 18:24:24 -0400

    resume

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
 import NavigationBar from './components/NavigationBar'
@@ -7,37 +6,38 @@ import { Link } from 'react-router-dom'
 
 
 
-function App() {
+function App ()
+{
   return (
 
-<div className="demo-big-content">
-    <Layout>
+    <div className="demo-big-content">
+      <Layout>
         <Header className="header-color" title="Nathan Rotach" scroll>
-            <Navigation>
-                 <Link to="/">Home</Link>
-                  <Link to="/aboutme">About Me</Link>
-                  <Link to="/projects">Projects</Link>
-                  <a href="https://docs.google.com/document/d/1afTp2aHgYrZoI5IR_QO6A8d-NFEAsHHpXcOkHMe6BUU/edit?usp=sharing" target="_blank">Resume</a>
-                  <Link to="/contact">Contact</Link>
-            </Navigation>
+          <Navigation>
+            <Link to="/">Home</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
         </Header>
         <Drawer title="Nathan Rotach">
-            <Navigation>
-               <Link to="/">Home</Link>
-               <Link to="/aboutme">About Me</Link>
-               <Link to="/projects">Projects</Link>
-               <a href="https://docs.google.com/document/d/1afTp2aHgYrZoI5IR_QO6A8d-NFEAsHHpXcOkHMe6BUU/edit?usp=sharing" target="_blank">Resume</a>
-               <Link to="/contact">Contact</Link>
-            </Navigation>
+          <Navigation>
+            <Link to="/">Home</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
         </Drawer>
         <Content>
           <div className="page-content" />
-                   <NavigationBar />
+          <NavigationBar />
 
 
         </Content>
-    </Layout>
-    {/* // <div className="demo-big-content">
+      </Layout>
+      {/* // <div className="demo-big-content">
     // <Layout>
     //     <Header className="header-color" scroll>
     //         <Navigation>
@@ -63,7 +63,7 @@ function App() {
     //     </Content>
     // </Layout>
     // </div> */}
-</div>
+    </div>
 
   );
 }
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
-export default class resume extends Component {
-    render() {
+export default class resume extends Component
+{
+    render ()
+    {
         return (
             <div>
                 <h1> resume </h1>
+                <embed src="../assets/NathanRotach.pdf" />>
             </div>
         )
     }
