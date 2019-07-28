ac4ecd5fa9742e961e4850e15db93146ae51a1f4
Nathan Rotach <sangunetsynn@gmail.com>
2019-07-28 10:14:12 -0400

    resume

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
+import { Document, Page } from "react-pdf"
 
 export default class resume extends Component
 {
-    render ()
+    state = { numPages: null, pageNumber: 1 }
+
+    onDocumentLoadSuccess = ( { numPages } ) =>
     {
-        return (
-            <div>
-                <h1> resume </h1>
-                {/* <embed src="../assets/NathanRotach.pdf" />> */}
-            </div>
-        )
+        this.setState( { numPages } )
     }
+
+    goToPrevPage = () =>
+        this.setState( state => ( { pageNumber: state.pageNumber - 1 } ) )
+    goToNextPage = () =>
+        this.setState( state => ( { pageNumber: state.pageNumber + 1 } ) )
+
+    render ()
+    const { pageNumber, numPages } = this.state
+{
+    return (
+        <div>
+            <h1> resume </h1>
+            {/* <embed src="../assets/NathanRotach.pdf" width="800px" height="2100px" /> */}
+            {/* <embed src="file_name.pdf"  /> */}
+            <div style={{ width: 600 }}>
+                <Document
+                    file="../assets/NathanRotach.pdf"
+                    onLoadSuccess={this.onDocumentLoadSuccesss}
+                >
+                    <Page pageNumber={pageNumber} width={600} />
+                </Document>
+            </div>
+        </div>
+    )
+}
 }
